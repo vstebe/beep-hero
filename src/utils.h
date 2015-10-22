0bba033f3ca4f4d3a9d46411534a19df6394a75a
Nicolas <nicolas.adam8@gmail.com>
2015-10-22 16:42:57 +0200

    add sheetParser modifications

---
+++ src/utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include<string>
+#include<vector>
+
+
+class Utils
+{
+public :
+    static std::vector<std::string> split(const std::string& s, const std::string& delimiter);
+
+};
+
+#endif // UTILS_H
